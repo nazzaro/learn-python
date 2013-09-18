1abbc920cc0aa5eb3510b797412e104da2b30312
Alessandro Nazzaro <alessandro.nazzaro.95@gmail.com>
2013-09-18 12:08:33 +0200

    ex 29-33

---
+++ ex28.py
@@ -0,0 +1,3 @@
+3 != 4 and not ("testing" != "test" or "Python" == "Python")
+3 != 4 True ("testing" != "test" or "Python" == "Python")
+"testing" != "test" is True: Trueand not (True or "Python" == "Python")
