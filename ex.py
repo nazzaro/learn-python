11f9ed686469fd9955d98da0775675cc3589739d
Alessandro Nazzaro <alessandro.nazzaro.95@gmail.com>
2013-09-23 16:17:58 +0200

    phone_keypad_skeleton

---
+++ ex.py
@@ -0,0 +1,17 @@
+lettere = "abcdefghijklmnopqrstuwxyz"
+n = 0
+
+
+for click in lettere:
+    n = n + 1
+    
+    if click == "s":
+        n = 4
+    
+    if click == "z":
+        n = 4
+    
+    elif n >= 4:
+        n = 1
+             
+    print click, n
