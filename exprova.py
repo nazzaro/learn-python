97a05de1bfcf7142052345e407bc8089000af1d9
nazzaro <alessandro.nazzaro.95@gmail.com>
2013-09-24 17:43:58 +0200

    Edit_exprova.py

--- exprova.py
+++ exprova.py
@@ -1,30 +1,34 @@
 from sys import argv
 
-script, filename = argv
 
-txt = open(filename)
-
-print txt.read()
-
-def tastiera():
-    
+# Aux functions
+def key_count(word):
     lettere = "abcdefghijklmnopqrstuwxyz"
-    
     n = 0
+    b = 0	
+    for char in lettere:
+		n = n + 1
+		char = click
+		if char == lettere:
+			b = n
+		else:		
+			if char == "s":
+				n = 4
+				b = b + 4
 
-    for click in lettere:
-        n = n + 1
-    
-    if click == "s":
-        n = 4
-    
-    if click == "z":
-        n = 4
-    
-    elif n >= 4:
-        n = 0 + 1
-             
-    print click, n
+			if char == "z":
+				n = 4
+				b = b + 4
+		
+			elif char >= 4:
+				n = 1
+				b += n
 
-print tastiera
-
+			print b
+		
+# Program init
+script, filename = argv
+for line in open(filename):
+    word = line.strip()
+    key_count(word)
+    print word
